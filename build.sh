6f92da1b32dfee83a71bdca4d155177b502aa007
Ajil Raju <ajilraju.m@gmail.com>
2022-04-13 08:31:45 +0530

    Changed author

--- build.sh
+++ build.sh
@@ -5,8 +5,14 @@ echo "Branch: ${GIT_BRANCH}"
 echo "Installing Dependencies..."
 npm install || exit 1
 
+echo "Removing old builds..."
+rm -rvf dist/*
+
 echo "Building applications..."
 ng build --base-href / || exit 1
+
+echo "Droping old apps from EC2"
+ssh root@3.142.145.171 'rm -rvf /var/www/html/*'
 
 echo "Deploying application on EC2..."
 rsync -avzP dist/keyshell/* root@3.142.145.171:/var/www/html/
